949b6868dec9cbe19b4094ba530a31fd5c806b49
frederick <00404919@uca.edu.sv>
2019-11-04 23:31:58 -0600

    Updates

---
+++ precache-manifest.668400ed38b6a5315c6d1359c4683e51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2f0eb5a88b1764aeaeee78b17804cb",
+    "url": "/Tarea-React/index.html"
+  },
+  {
+    "revision": "8900086c420cfc3f4c16",
+    "url": "/Tarea-React/static/css/2.a96bfb44.chunk.css"
+  },
+  {
+    "revision": "8900086c420cfc3f4c16",
+    "url": "/Tarea-React/static/js/2.529a8270.chunk.js"
+  },
+  {
+    "revision": "8e6de6d0a773a694f249",
+    "url": "/Tarea-React/static/js/main.6825d5ab.chunk.js"
+  },
+  {
+    "revision": "fcf0100e24d8c9925afb",
+    "url": "/Tarea-React/static/js/runtime-main.dd8e2793.js"
+  }
+]);
